bc98368da33960e6d3467a53a487825a4974c9b9
Kate Hillyard <katehilly1212@gmail.com>
2024-04-10 16:22:00 -0600

    major styling upgrades and additional update and insert method implemented into app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 // Libraries
 const express = require("express");
+const { request } = require("http");
 const multer = require("multer");
 const mysql = require("mysql2");
+const { check, checkSchema, validationResult } = require("express-validator");
 
 // Setup defaults for script
 const app = express();
@@ -14,6 +16,21 @@ const connection = mysql.createConnection({
   password: "nJ1OhMdOUPZw4qyZsUe5GweqeJIlVqYtAlq",
   database: "KATEHILLYARD",
 });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(express.json());
+
+app.options("/*", function (req, res, next) {
+  res.sendStatus(200);
+});
+
+let goodFormValues = [];
 
 // The * in app.* needs to match the method type of the request
 app.post("/", upload.none(), (request, response) => {
@@ -108,6 +125,165 @@ INNER JOIN dog_stats ON dog_breeds.id = dog_stats.id`;
     }
   });
 });
+
+//Insert Javascript here
+app.post(
+  "/INSERT",
+  upload.none(),
+  //name validation
+  check("name", "You gotta have a name for your puppo.").isLength({ min: 3 }),
+  //breed validation
+  check("breed", "Please select a breed for you puppers.").isIn([
+    "''", //empty string to prevent default value
+    "toy",
+    "work",
+    "herd",
+    "sport",
+  ]),
+  //fur validation
+  check("fur", "Describe your doggos fur.").isIn([
+    "''", //empty string to prevent default value
+    "smooth",
+    "rough",
+    "cord",
+    "doub",
+    "silk",
+    "curl",
+    "wire",
+    "hless",
+  ]),
+  //color validation
+  check("color", "Give your puppy some color!").isLength({ min: 3 }),
+  //energy validation
+  check("energy", "Tell us how jazzed your fluffers is.").isIn([
+    "''", //empty string to prevent default value
+    "low",
+    "med",
+    "high",
+  ]),
+  //size validation
+  check("size", "How big or small is the dawg?").isInt({ min: 1, max: 300 }),
+
+  (request, response) => {
+    const insertSql = `INSERT INTO dog_breeds (name, breed, fur, color, energy, size) 
+                          VALUES (?, ?, ?, ?, ?, ?)`
+    let queryParams = [
+      request.body.name,
+      request.body.breed,
+      request.body.fur,
+      request.body.color,
+      request.body.energy,
+      parseInt(request.body.size)
+    ];
+
+    const errors = validationResult(request)
+    goodFormValues.push({ ...request.body });
+
+    connection.query(insertSql, queryParams, (error, result) => {
+      if (error) {
+        console.log(error);
+        return response
+          .status(450) //Error code when something goes wrong with the server
+          .setHeader("Access-Control-Allow-Origin", "*") //Prevent CORS error
+
+          .json({
+            message: "Something went wrong with the server.",
+            errors: errors.array(),
+          });
+      } else {
+        //Default response object
+        response
+          .status(250)
+          .setHeader("Access-Control-Allow-Origin", "*") //Prevent CORS error
+          .json({
+            message: "Form submission was succesful!",
+            goodFormValues: goodFormValues
+          });
+      }
+    });
+  }
+);
+
+//update
+
+app.post(
+  "/UPDATE",
+  upload.none(),
+
+  //name validation
+  check("name", "You gotta have a name for your puppo.").isLength({ min: 3 }),
+  //breed validation
+  check("breed", "Please select a breed for you puppers.").isIn([
+    "''", //empty string to prevent default value
+    "toy",
+    "work",
+    "herd",
+    "sport",
+  ]),
+  //fur validation
+  check("fur", "Describe your doggos fur.").isIn([
+    "''", //empty string to prevent default value
+    "smooth",
+    "rough",
+    "cord",
+    "doub",
+    "silk",
+    "curl",
+    "wire",
+    "hless",
+  ]),
+  //color validation
+  check("color", "Give your puppy some color!").isLength({ min: 3 }),
+  //energy validation
+  check("energy", "Tell us how jazzed your fluffers is.").isIn([
+    "''", //empty string to prevent default value
+    "low",
+    "med",
+    "high",
+  ]),
+  //size validation
+  check("size", "How big or small is the dawg?").isInt({ min: 1, max: 300 }),
+
+  (request, response) => {
+    const updateSql =
+      "UPDATE dog_breeds name = ?, breed = ?, fur = ?, color = ?, energy = ?, size = ? WHERE id = ?";
+
+    let queryParams = [
+      request.body.name,
+      request.body.breed,
+      request.body.fur,
+      request.body.color,
+      request.body.energy,
+      parseInt(request.body.size)
+    ];
+
+    const errors = validationResult(request)
+    goodFormValues.push({ ...request.body });
+
+    connection.query(updateSql, queryParams, (error, result) => {
+      if (error) {
+        console.log(error);
+        return response
+          .status(450) //Error code when something goes wrong with the server
+          .setHeader("Access-Control-Allow-Origin", "*") //Prevent CORS error
+
+          .json({
+            message: "Something went wrong with the server.",
+            errors: errors.array(),
+          });
+      } else {
+        //Default response object
+        response
+          .status(250)
+          .setHeader("Access-Control-Allow-Origin", "*") //Prevent CORS error
+          .json({
+            message: "Form submission was succesful!",
+            goodFormValues: goodFormValues
+          });
+      }
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
