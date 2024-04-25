df2d5cdd4a2a1e20768fc41eca4660e9554406e3
Kate Hillyard <katehilly1212@gmail.com>
2024-04-25 12:59:37 -0600

    so much

--- server/app.js
+++ server/app.js
@@ -1,143 +1,44 @@
 // Libraries
 const express = require("express");
-const { request } = require("http");
 const multer = require("multer");
+const cors = require('cors');
 const mysql = require("mysql2");
-const dogs = require("./model/dogs");
 const { check, checkSchema, validationResult } = require("express-validator");
+const connection = require('./model/connection.js');
+const dogs = require('./model/dogs.js');
 
 // Setup defaults for script
 const app = express();
+app.use(cors());
 app.use(express.static("public"));
+
 const upload = multer();
 const port = 80; // Default port to HTTP server
-const connection = mysql.createConnection({
-  //http://54.190.23.106/phpMyAdmin/index.php?route=/
-  host: "student-databases.cvode4s4cwrc.us-west-2.rds.amazonaws.com",
-  user: "KATEHILLYARD",
-  password: "nJ1OhMdOUPZw4qyZsUe5GweqeJIlVqYtAlq",
-  database: "KATEHILLYARD",
-});
-
-app.use((req, res, next) => {
-  res;
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
-app.use(express.json());
-
-app.options("/*", function (req, res, next) {
-  res.sendStatus(200);
-});
-
-let goodFormValues = [];
-
-// The * in app.* needs to match the method type of the request
-app.post("/", upload.none(), (request, response) => {
-  // SELECT statement variables
-  let selectSql = `SELECT 
-  dog_breeds.name, 
-  dog_breeds.breed, 
-  dog_breeds.fur, 
-  dog_breeds.color, 
-  dog_breeds.energy, 
-  dog_breeds.size,
-  dog_stats.shedding_level,
-  dog_stats.dog_kindness,
-  dog_stats.child_kindness,
-  dog_stats.drool_level,
-  dog_stats.bark_level,
-  dog_stats.train_level
-FROM dog_breeds
-INNER JOIN dog_stats ON dog_breeds.id = dog_stats.id`;
-
-  let whereStatements = [];
-  let queryParams = [];
-  let orderByStatements = [];
 
 
 
-
-  // Filtering based on form inputs
-  console.log(request.body);
-
-  if (
-    typeof request.body.breed !== "undefined" &&
-    request.body.breed !== "none"
-  ) {
-    whereStatements.push(`breed = ?`);
-    queryParams.push(request.body.breed);
-  }
-  if (typeof request.body.fur !== "undefined" && request.body.fur !== "none") {
-    whereStatements.push(`fur = ?`);
-    queryParams.push(request.body.fur);
-  }
-  if (
-    typeof request.body.color !== "undefined" &&
-    request.body.color.length > 0
-  ) {
-    whereStatements.push(`color LIKE ?`);
-    queryParams.push("%" + request.body.color + "%");
-  }
-  if (
-    typeof request.body.energy !== "undefined" &&
-    request.body.energy !== "none"
-  ) {
-    whereStatements.push(`energy = ?`);
-    queryParams.push(request.body.energy);
-  }
-  if (
-    typeof request.body.size !== "undefined" &&
-    request.body.size.length > 0
-  ) {
-    whereStatements.push(`size = ?`);
-    queryParams.push(request.body.size);
-  }
-
-  // Dynamically add WHERE expressions to SELECT statements if needed
-  if (whereStatements.length > 0) {
-    selectSql += " WHERE " + whereStatements.join(" AND ");
-  }
-
-  //Dynamically add ORDER BY expressions to SELECT statements if needed
-  if (orderByStatements.length > 0) {
-    selectSql = selectSql + " ORDER BY " + orderByStatements.join(", ");
-  }
-
-  if (
-    typeof request.body.limit !== "undefined" &&
-    request.body.limit > 0 &&
-    request.body.limit <= 100 // Ensure limit is within a reasonable range
-  ) {
-    selectSql += " LIMIT ?";
-    queryParams.push(parseInt(request.body.limit)); // Push the limit value to queryParams
-  }
-
-  connection.query(selectSql, queryParams, (error, result) => {
-    if (error) {
+app.get('/dogs/', upload.none(), async (request, response) => {
+  //SELECT statement variables
+  
+  try{
+      let result = await dogs.getAll(request.query);
+      console.log({ data: result });
+      return response
+          .status(200)
+          .json({data: result});
+  } catch (error) {
       console.log(error);
       return response
-        .status(500) // Error code when something goes wrong with the server
-        .json({ message: "Something went wrong with the server." });
-    } else {
-      // Default response object
-      response
-        // Prevent CORS error
-        .json({ data: result });
-    }
-  });
+          .status(500) //Error code when something goes wrong with the server
+          .json({ message: 'Something went wrong with the server.' });
+  }
 });
 
 
 
-//Insert 
-app.post(
-  "/dogs/",
-  upload.none(),
-  //name validation
-  check("name", "You gotta have a name for your puppo.").isLength({ min: 3 }),
+app.post('/dogs/', upload.none(),
+
+check("name", "You gotta have a name for your puppo.").isLength({ min: 3 }),
   //breed validation
   check("breed", "Please select a breed for you puppers.").isIn([
     "''", //empty string to prevent default value
@@ -169,32 +70,71 @@ app.post(
   ]),
   //size validation
   check("size", "How big or small is the dawg?").isInt({ min: 1, max: 300 }),
+
+
 
   async (request, response) => {
-    //let errors = validationResult(request);
-    //if (!errors.isEmpty()) {...}
-    try {
-      await dogs.insert(request.body);
-    } catch (error) {
-      console.log(error);
-      return response
-        .status(500)
-        .json({ message: "Something went wrong with the server." });
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return response
+            .status(400)
+            .json({
+                message: 'Request fields are invalid.',
+                errors: errors.array(),
+            });
     }
 
-    response.json({ message: "Form submission was succesful!" });
-  }
-);
+    try{
+        await dogs.insert(request.body);
+        return response
+            .status(200)
+            .json({ message: 'New weapon created!' });
+    } catch (error) {
+        console.log(error);
+        return response
+            .status(500) //Error code when something goes wrong with the server
+            .json({ message: 'Something went wrong with the server.' });
+    }
+    //response.json({ message: 'Form submission was succesful!' });
+});
+
+
+//JSON of values from database
+app.get('/dogs/:dogId', upload.none(),
+    async (request, response) => {
+        const errors = validationResult(request)
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            return response
+                .status(400)
+                .json({
+                    message: 'Request fields are invalid.',
+                    errors: errors.array(),
+                });
+        }
+        
+        try{
+            const result = await dogs.get(request.params.dogId);
+            console.log({ data: result });
+            return response
+                .status(200)
+                .json({ data: result });
+        }catch (error) {
+            console.log(error);
+            return response
+                .status(500) //Error code when something goes wrong with the server
+                .setHeader('Access-Control-Allow-Origin', '*') //Prevent CORS error
+                .json({ message: 'Something went wrong with the server.' });
+        }
+        //response.json({ data: result });
+        //console.log({ data: result });
+    });
 
 
 
-//update
-app.post(
-  "/UPDATE",
-  upload.none(),
-
-  //name validation
-  check("name", "You gotta have a name for your puppo.").isLength({ min: 3 }),
+    app.put('/dogs/:dogId', upload.none(),
+    check("name", "You gotta have a name for your puppo.").isLength({ min: 3 }),
   //breed validation
   check("breed", "Please select a breed for you puppers.").isIn([
     "''", //empty string to prevent default value
@@ -227,76 +167,68 @@ app.post(
   //size validation
   check("size", "How big or small is the dawg?").isInt({ min: 1, max: 300 }),
 
-  (request, response) => {
-    const updateSql =
-      "UPDATE dog_breeds name = ?, breed = ?, fur = ?, color = ?, energy = ?, size = ? WHERE id = ?";
 
-    let queryParams = [
-      request.body.name,
-      request.body.breed,
-      request.body.fur,
-      request.body.color,
-      request.body.energy,
-      parseInt(request.body.size),
-    ];
-
-    const errors = validationResult(request);
-    goodFormValues.push({ ...request.body });
-
-    connection.query(updateSql, queryParams, (error, result) => {
-      if (error) {
-        console.log(error);
-        return (
-          response
-            .status(450) //Error code when something goes wrong with the server
-            //Prevent CORS error
-
+  
+  async (request, response) => {
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return response
+            .status(400)
+            .setHeader('Access-Control-Allow-Origin', '*')
             .json({
-              message: "Something went wrong with the server.",
-              errors: errors.array(),
-            })
-        );
-      } else {
-        //Default response object
-        response
-          .status(250)
-          //Prevent CORS error
-          .json({
-            message: "Form submission was succesful!",
-            goodFormValues: goodFormValues,
-          });
-      }
-    });
-  }
-);
-
-
-
-
-//delete
-app.put("/DELETE", upload.none(), (request, response) => {
-  const errors = validationResult(request);
-  if (!errors.isEmpty()) {
-    return response.status(400).json({ errors: errors.array() });
-  }
-  let data = request.body;
-
-  let deleteSql = `DELETE FROM dog_breeds WHERE id = ?`;
-  let values = [data.id];
-
-  connections.query(deleteSql, values, (error, result) => {
-    if (error) {
-      console.log(error);
-      return response
-        .status(500)
-
-        .json({ message: "Something went wrong with the server." });
-    } else {
-      response.json({ data: result });
+                message: 'Request fields are invalid.',
+                errors: errors.array(),
+            });
     }
-  });
+    try{
+        request.body.dogId = request.params.dogId;
+        await dogs.edit(request.body);
+        return response
+            .status(200)
+            .json({ message: 'Puppy Added!' });
+    } catch (error) {
+            console.log(error);
+            return response
+                .status(500) //Error code when something goes wrong with the server
+                .setHeader('Access-Control-Allow-Origin', '*') //Prevent CORS error
+                .json({ message: 'Something went wrong with the server.' });
+    }
+    //Default response object
+    //response.status(200).json({ message: 'Form submission was succesful!' });
+});
+
+
+app.delete('/dogs/:dogId', async (request, response) => {
+
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return response
+            .status(400)
+            .setHeader('Access-Control-Allow-Origin', '*')
+            .json({
+                message: 'Request fields or files are invalid.',
+                errors: errors.array(),
+            });
+    }
+    try{
+        await dogs.deleteById(request.params.weaponID);
+        return response
+            .status(200)
+            .json({ message: 'Puppy Added!' });
+    } catch (error) {
+        console.log(error);
+        return response
+            .status(500) //Error code when something goes wrong with the server
+            .json({ message: 'Something went wrong with the server.' });
+    }
+    //Default response object
+    //response.json({ message: 'Puppy Added!' });
+        
 });
 
 app.listen(port, () => {
-  console.log(`Application app listening at http://localhost:${port}`);
-});
+    console.log(`Application listening at http://localhost:${port}`);
+})
+
--- server/model/dogs.js
+++ server/model/dogs.js
@@ -1,29 +1,179 @@
-const connection = require("./connection");
+const connection = require('./connection.js');
 
-async function get(id) {}
-async function getAll(parameters = {}) {}
-async function insert(parameters = {}) {
-  const insertSql = `INSERT INTO dog_breeds (name, breed, fur, color, energy, size) 
-        VALUES (?, ?, ?, ?, ?, ?)`;
-  let queryParams = [
-    parameters.name,
-    parameters.breed,
-    parameters.fur,
-    parameters.color,
-    parameters.energy,
-    parseInt(parameters.size),
-  ];
+async function get(id) {
+  const getSql = 'SELECT * FROM dog_breeds WHERE id = ?';
+  let queryParams = [id];
+
+  console.log(getSql);
+  console.log(queryParams);
+
+  return await connection.query(insertSql, queryParams);
+
 }
 
-return await connection.query(insertSql, queryParams);
+async function getAll(parameters = {}) {
+  let selectSql = `SELECT 
+     dog_breeds.id AS dogId,
+     dog_breeds.name, 
+     dog_breeds.breed, 
+     dog_breeds.fur, 
+     dog_breeds.color, 
+     dog_breeds.energy, 
+     dog_breeds.size,
+     dog_stats.shedding_level,
+     dog_stats.dog_kindness,
+     dog_stats.child_kindness,
+     dog_stats.drool_level,
+     dog_stats.bark_level,
+     dog_stats.train_level
+   FROM dog_breeds
+   INNER JOIN dog_stats ON dog_breeds.id = dog_stats.id`;
 
-async function edit(parameters = {}) {}
-async function deleteById(id) {}
+  let whereStatements = [];
+  let queryParams = [];
+  let orderByStatements = [];
+
+
+  //Filtering based on form inputs
+  console.log(request.body);
+
+  if (
+    typeof request.body.breed !== "undefined" &&
+    request.body.breed !== "none"
+  ) {
+    whereStatements.push(`breed = ?`);
+    queryParams.push(request.body.breed);
+  }
+  if (typeof request.body.fur !== "undefined" && request.body.fur !== "none") {
+    whereStatements.push(`fur = ?`);
+    queryParams.push(request.body.fur);
+  }
+  if (
+    typeof request.body.color !== "undefined" &&
+    request.body.color.length > 0
+  ) {
+    whereStatements.push(`color LIKE ?`);
+    queryParams.push("%" + request.body.color + "%");
+  }
+  if (
+    typeof request.body.energy !== "undefined" &&
+    request.body.energy !== "none"
+  ) {
+    whereStatements.push(`energy = ?`);
+    queryParams.push(request.body.energy);
+  }
+  if (
+    typeof request.body.size !== "undefined" &&
+    request.body.size.length > 0
+  ) {
+    whereStatements.push(`size = ?`);
+    queryParams.push(request.body.size);
+  }
+
+  // Dynamically add WHERE expressions to SELECT statements if needed
+  if (whereStatements.length > 0) {
+    selectSql += " WHERE " + whereStatements.join(" AND ");
+  }
+
+  //Dynamically add ORDER BY expressions to SELECT statements if needed
+  if (orderByStatements.length > 0) {
+    selectSql = selectSql + " ORDER BY " + orderByStatements.join(", ");
+  }
+
+  if (
+    typeof request.body.limit !== "undefined" &&
+    request.body.limit > 0 &&
+    request.body.limit <= 100 // Ensure limit is within a reasonable range
+  ) {
+    selectSql += " LIMIT ?";
+    queryParams.push(parseInt(request.body.limit)); // Push the limit value to queryParams
+  }
+
+  console.log(selectSql);
+  return await connection.query(insertSql, queryParams);
+}
+
+
+
+async function insert(parameters = {}) {
+
+  let insertSql = 'INSERT INTO dog_breeds (name, breed, fur, color, energy, size) VALUES (?, ?, ?, ?, ?, ?) ';
+  let queryParams = [];
+
+  if (typeof parameters.name !== 'undefined') {
+    queryParameters.push(parameters.name);
+  }
+  if (typeof parameters.breed !== 'undefined') {
+    queryParameters.push(parameters.breed);
+  }
+  if (typeof parameters.fur !== 'undefined') {
+    queryParameters.push(parameters.fur);
+  }
+  if (typeof parameters.color !== 'undefined') {
+    queryParameters.push(parameters.color);
+  }
+  if (typeof parameters.energy !== 'undefined') {
+    queryParameters.push(parameters.energy);
+  }
+  if (typeof parameters.size !== 'undefined') {
+    queryParameters.push(parameters.size);
+  }
+
+  return await connection.query(insertSql, queryParams);
+}
+
+
+
+async function edit(parameters = {}) {
+  let updateSql = 'UPDATE dog_breeds SET name = ?, breed = ?, fur = ?, color = ?, energy = ?, size = ?  WHERE  id = ?';
+  let queryParams = [];
+
+  if (typeof parameters.name !== 'undefined') {
+    queryParameters.push(parameters.name);
+  }
+  if (typeof parameters.breed !== 'undefined') {
+    queryParameters.push(parameters.breed);
+  }
+  if (typeof parameters.fur !== 'undefined') {
+    queryParameters.push(parameters.fur);
+  }
+  if (typeof parameters.color !== 'undefined') {
+    queryParameters.push(parameters.color);
+  }
+  if (typeof parameters.energy !== 'undefined') {
+    queryParameters.push(parameters.energy);
+  }
+  if (typeof parameters.size !== 'undefined') {
+    queryParameters.push(parameters.size);
+  }
+
+  queryParams.push(parameters.dogId);
+
+  console.log(queryParams);
+  console.log(updateSql);
+
+  return await connection.query(updateSqlSql, queryParams);
+
+}
+
+
+async function deleteById(id) {
+
+  const deleteSql = `DELETE FROM dog_breeds WHERE id =?`
+  let queryParams = [id];
+
+  console.log(deleteSql);
+  console.log(queryParams);
+
+  return await connection.query(deleteSql, queryParams);
+
+
+}
 
 module.exports = {
   get,
   getAll,
   insert,
   edit,
-  deleteById,
-};
+  deleteById
+}
